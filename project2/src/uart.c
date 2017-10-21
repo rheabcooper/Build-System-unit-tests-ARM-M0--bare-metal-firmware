81e10c536c87fc5eac1e1911e5169990421ea927
rheabcooper <31560258+rheabcooper@users.noreply.github.com>
2017-10-21 09:09:39 +0530

    Update uart.c

--- project2/src/uart.c
+++ project2/src/uart.c
@@ -1,4 +1,5 @@
-/**
+
+	/**
  * @file uart.c 
  * @brief This file contains the functions for UART interface
  *			of the Freedom Freescale board.  
@@ -16,7 +17,7 @@
 
 void UART_configure()
 {
-	SIM_SCG4|=SIM_SCGC4_UART0_MASK;
+	SIM_SCG4|=SIM_SCGC4_UART0_MASK;    //enabling clock to UART0             
 	SIM_SOPT2|=SIM_SCGC4_UART0_MASK & MCGFLLCLK;
 	UART0_C2 = 0x00;
 	UART0_BDH=BDH;
@@ -24,22 +25,22 @@ void UART_configure()
 	UART0_C4=UART0_C4_OSR_MASK & OSR;
 	UART0_C1=0x00;
 	UART0_C2=UART0_C2_TE_MASK|UART0_C2_RE_MASK|UART0_C2_RIE_MASK|UART0_C2_TIE_MASK;
-	SIM_SCG5|=SIM_SCGC5_PORTA_MASK;
-	PORTA_PCR1=0x02;
-	PORTA_PCR2=0x02;
+	SIM_SCG5|=SIM_SCGC5_PORTA_MASK;       //enabling clock to Port A 
+	PORTA_PCR1=0x02;         //alternate function for PTA1 -UART0_RX
+	PORTA_PCR2=0x02;         //alternate function for PTA1 -UART0_RX
 	NVIC->ISER[0] |= 0x00001000;
 }
 
 void UART_send(uint8_t *data)
 {	
-	while(!(UART0_S1 & 0x80)){ }
+	while(!(UART0_S1 & UART0_S1_TDRF_MASK)){ }
 	UART0_D = *data;
 }
 
 void UART_receive(uint8_t *data)
 {	
 	
-	while(!(UART0_S1 & 0x20)){ }
+	while(!(UART0_S1 & UART0_S1_RDRF_MASK)){ }
 	*data=UART0_D;
 }
 
@@ -47,7 +48,7 @@ void UART_send_n(uint32_t *data,uint32_t count)
 {
 	for(i=0;i<=count;i++)
 	{
-		while(!(UART0_S1 & 0x80)){ }
+		while(!(UART0_S1 & UART0_S1_TDRF_MASK)){ }
 		UART0_D=*(data+i);
 	}
 	
@@ -57,29 +58,39 @@ void UART_receive_n(uint32_t *data,uint32_t count)
 {	
 	for(i=0;i<=count;i++)
 	{
-		while(!(UART0_S1 & 0x20)){ }
+		while(!(UART0_S1 & UART0_S1_RDRF_MASK)){ }
 		*(data+i)=UART0_D;
 	}
 }
 
 void UART_IRQhandler()
-{	/*The following should be in the main function
-	_disable_irq(); // global disable IRQs //
+{	
+	/*In main:
+	__disable_irq(); // global disable IRQs
 	UART_configure();
-	__enable_irq();	// global enable IRQs //
-	while(1){}  */
-	         
-	//IF RDRF==1
-	if(UART0_S1 | 0xDF )
+	__enable_irq();
+	while(1);
+	*/
+	__disable_irq(); // global disable IRQs
+	if((UART0_C2 & UART0_C2_RIE_MASK)!=0)        
 	{
-		UART_receive();
-	//clear receive interrupt flag -ISFR
-		PORTA_ISFR| =PORT_ISFR_ISF_MASK & PTA2_ISFR_MASK;                      
+		if(UART0_S1 & UART0_S1_RDRF_MASK) !=0){
+			UART_receive();
+		}
+		UART0_C2 &= ~UART0_C2_RIE_MASK;	
 	}
 	//IF TDRF==1
-	if(UART0_S1 | 0x7F ) 
+	 else if((UART0_C2 & UART0_C2_TIE_MASK)!=0) 
 	{
-		UART_send();
-		PORTA_ISFR| =PORT_ISFR_ISF_MASK & PTA1_ISFR_MASK; 
-    }
+		if(UART0_S1 & UART0_S1_TDRF_MASK) !=0){
+			UART_send();
+		} 
+		UART0_C2 &= ~UART0_C2_TIE_MASK;
+	}	
+	__enable_irq(); // global disable IRQs
+	
 }
+
+
+
+	

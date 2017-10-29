a5ed1b8943edaf3a6bfaf37071d08bfeb62d8b31
rheabcooper <31560258+rheabcooper@users.noreply.github.com>
2017-10-29 21:26:55 +0530

    Update memory_test.c

--- project2/test/memory_test.c
+++ project2/test/memory_test.c
@@ -23,25 +23,103 @@
 #define ODD_LENGTH (5)
 #define EVEN_LENGTH (6)
 #define VALUE (1)
+#define TEST_MEMMOVE_LENGTH (16)
+#define LENGTH (32)
+
+uint8_t array[LENGTH];
 
 void test_memmove_invalid_pointers(void **state)
 {
-	skip();	
+	uint8_t *src=NULL;
+	uint8_t *dst=NULL;
+	memory_status status=my_memmove(src, dst, TEST_MEMMOVE_LENGTH);
+	assert_int_equal(status,NULL_POINTER);	
 }
 	
 void test_memmove_no_overlap(void **state)
 {
-	assert_int_equal(1,1);	
+	  uint8_t *src = &array[0];
+	  uint8_t *dst = &array[16];
+  	  uint8_t i;
+  /* Initialize the set to test values */
+  	for( i = 0; i < LENGTH; i++)
+  	{
+    		array[i] = i;
+  	}
+
+  
+  	my_memmove(src, dst, TEST_MEMMOVE_LENGTH);
+ 
+	uint8_t flag=ZERO;
+  	for (i = 0; i < TEST_MEMMOVE_LENGTH; i++)
+  	{
+    		if (array[i + 16] != i)
+    		{
+      			flag++;
+			break;
+    		}
+  	}
+	if(flag==ZERO)
+		memory_status status=SUCCESS;
+	assert_int_equal(status,SUCCESS);
 }
 
 void test_memmove_src_in_dst_overlap(void **state)
 {
-	assert_int_not_equal(1,0);	
+	  uint8_t *src = &array[8];
+	  uint8_t *dst = &array[0];
+  	  uint8_t i;
+  /* Initialize the set to test values */
+  	for( i = 0; i < LENGTH; i++)
+  	{
+    		array[i] = i;
+  	}
+
+  
+  	my_memmove(src, dst, TEST_MEMMOVE_LENGTH);
+ 
+	uint8_t flag=ZERO;
+  	for (i = 0; i < TEST_MEMMOVE_LENGTH; i++)
+  	{
+    		if (array[i] != array[i+8])
+    		{
+      			flag++;
+			break;
+    		}
+  	}
+	if(flag==ZERO)
+		memory_status status=SUCCESS;
+	assert_int_equal(status,SUCCESS);	
+	
+	
 }
 
 void test_memmove_dst_in_src_overlap(void **state)
 {
-	assert_int_equal(1,1);	
+	  uint8_t *src = &array[0];
+	  uint8_t *dst = &array[8];
+  	  uint8_t i;
+  /* Initialize the set to test values */
+  	for( i = 0; i < LENGTH; i++)
+  	{
+    		array[i] = i;
+  	}
+
+  
+  	my_memmove(src, dst, TEST_MEMMOVE_LENGTH);
+ 
+	uint8_t flag=ZERO;
+  	for (i = 0; i < TEST_MEMMOVE_LENGTH; i++)
+  	{
+    		if (array[i + 8] != i)
+    		{
+      			flag++;
+			break;
+    		}
+  	}
+	if(flag==ZERO)
+		memory_status status=SUCCESS;
+	assert_int_equal(status,SUCCESS);	
 }
 
 void test_memset_invalid_pointers(void **state)
@@ -52,7 +130,12 @@ void test_memset_invalid_pointers(void **state)
 }
 
 void test_memset_check_set(void **state)
-{
+{	
+	uint8_t i;
+	for( i = 0; i < LENGTH; i++)
+  	{
+    		array[i] = i;
+  	}
 	uint8_t *src=&array[0];
 	memory_status status = my_memset(src,ODD_LENGTH,VALUE);
 	assert_int_equal(status,SUCCESS);	
@@ -60,6 +143,7 @@ void test_memset_check_set(void **state)
 
 void test_memzero_invalid_pointers(void **state)
 {
+	
 	uint8_t *src=NULL;
 	memory_status status = my_memzero(src,ODD_LENGTH);
 	assert_int_equal(status,NULL_POINTER);
@@ -67,6 +151,11 @@ void test_memzero_invalid_pointers(void **state)
 
 void test_memzero_check_set(void **state)
 {	
+	uint8_t i;
+	for( i = 0; i < LENGTH; i++)
+  	{
+    		array[i] = i;
+  	}
 	uint8_t *src=&array[0];
 	memory_status status = my_memzero(src,ODD_LENGTH);
 	assert_int_equal(status,SUCCESS);		
@@ -81,16 +170,41 @@ void test_reverse_invalid_pointers(void **state)
 
 void test_reverse_check_odd_reverse(void **state)
 {
+	uint8_t i;
+	uint8_t flag=ZERO;
+	uint8_t copy[LENGTH];
+	for( i = 0; i < LENGTH; i++)
+  	{
+    		array[i] = i;
+  	}
 	uint8_t *src=&array[0];
-	memory_status status = my_reverse(src,ODD_LENGTH);
-	assert_int_equal(status,SUCCESS);	
+	my_reverse(src,ODD_LENGTH);
+	for(i=0;i<ODD_LENGTH;i++)
+	{
+		if(array[i]!=copy[ODD_LENGTH-i])
+			flag++;
+	}
+	assert_int_equal(flag,ZERO);
 }
 
 void test_reverse_check_even_reverse(void **state)
 {
+	uint8_t i;
+	uint8_t flag=ZERO;
+	uint8_t copy[LENGTH];
+	for( i = 0; i < LENGTH; i++)
+  	{
+    		array[i] = i;
+		copy[i]=i;
+  	}
 	uint8_t *src=&array[0];
-	memory_status status = my_reverse(src,EVEN_LENGTH);
-	assert_int_equal(status,SUCCESS);	
+	my_reverse(src,EVEN_LENGTH);
+	for(i=0;i<EVEN_LENGTH;i++)
+	{
+		if(array[i]!=copy[EVEN_LENGTH-i])
+			flag++;
+	}
+	assert_int_equal(flag,ZERO);	
 }
 
 void test_reverse_check_characters(void **state)

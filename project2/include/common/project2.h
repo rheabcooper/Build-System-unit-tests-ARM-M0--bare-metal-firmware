5cea4494e36719f0ee750b1956e0ea7edd30e5ad
rheabcooper <31560258+rheabcooper@users.noreply.github.com>
2017-10-30 03:44:45 +0530

    Update project2.h

--- project2/include/common/project2.h
+++ project2/include/common/project2.h
@@ -24,6 +24,28 @@ uint8_t analyse = 0;
  *
  * @return void
  */
+
+uint32_t CB_TX_length=576;
+uint32_t CB_RX_length=16;
+int32_t alphabets = 0;
+int32_t numbers = 0;
+int32_t punctuations = 0;
+int32_t misc = 0;
+uint8_t string_for_alp[]="Number of alphabets is : \n\r";
+uint8_t string_for_num[]="Number of numbers is : \n\r";
+uint8_t string_for_punc[]="Number of punctuations is : \n\r";
+uint8_t string_for_misc[]="Number of miscellaneous characters is : \n\r";
+
+
+
+void transmit_integer(int32_t integer_value);
+void transmit_string(uint8_t * data);
+void transmit_char(uint8_t * data);
+void transmit_char_host(uint8_t * data);
+void transmit_string_host(uint8_t * data);
+void transmit_integer_host(in32_t integer_value);
+void data_analysis();
+
 void project2(void);
 
 #endif /* __PROJECT2_H__ */

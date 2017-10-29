f32a504d442fc3902edc41d57a1d6b683dc5076f
rheabcooper <31560258+rheabcooper@users.noreply.github.com>
2017-10-29 08:35:22 +0530

    Update uart.c

--- project2/src/uart.c
+++ project2/src/uart.c
@@ -13,6 +13,7 @@
 
 #include "uart.h"
 #include "cirbuf.h"
+#include "project2.h"
 
 void UART_configure()
 {
@@ -39,74 +40,79 @@ void UART_configure()
 	PORTA_PCR2 = PORT_PCR_MUX(2);         /* alternate function for PTA1 -UART0_RX */
 
 	/* enabling the transmitting, receiving, and receive/transmit interrupts */
-	UART0_C2 = UART0_C2_TE_MASK | UART0_C2_RE_MASK | UART0_C2_RIE_MASK | UART0_C2_TIE_MASK;
+	UART0_C2 = UART0_C2_TE_MASK | UART0_C2_RE_MASK | UART0_C2_RIE_MASK;
 
 	/* Interrupt enabling */
 	NVIC->ISER[0] |= 0x00001000;
 }
 
-void UART_send(uint8_t *data)
+void UART_send(uint8_t data)
 {	
-	while(!(UART0_S1 & UART0_S1_TDRE_MASK)){ }
-	UART0_D = *data;
+	while(!(UART0_S1 & UART0_S1_TDRF_MASK)){ }
+	UART0_D = data;
 }
 
-void UART_receive(uint8_t *data)
+uint8_t UART_receive()
 {	
-	
+	uint8_t c;
 	while(!(UART0_S1 & UART0_S1_RDRF_MASK)){ }
-	*data=UART0_D;
+	
+	c=UART0_D;
 }
 
-void UART_send_n(uint32_t *data, uint32_t length)
+void UART_send_n(uint8_t *data,uint8_t count)
 {
-	uint32_t i;
-
-	for(i=0; i<=length; i++)
+	for(uint8_t i=0;i<=count;i++)
 	{
-		while(!(UART0_S1 & UART0_S1_TDRE_MASK)){ }
+		while(!(UART0_S1 & UART0_S1_TDRF_MASK)){ }
 		UART0_D=*(data+i);
 	}
 	
 }	
 
-void UART_receive_n(uint32_t *data, uint32_t length)
+void UART_receive_n(uint8_t *data,uint8_t count)
 {	
-	uint32_t i;
-
-	for(i=0; i<=length; i++)
+	for(uint8_t i=0;i<=count;i++)
 	{
 		while(!(UART0_S1 & UART0_S1_RDRF_MASK)){ }
 		*(data+i)=UART0_D;
 	}
 }
 
-void UART_IRQhandler()
+void UART0_IRQhandler()
 {	
-
-
-	__disable_irq(); /* global disable IRQs */
-	if(0 != (UART0_C2 & UART0_C2_RIE_MASK))        
-	{
-		if(0 != (UART0_S1 & UART0_S1_RDRF_MASK))
-		{
-			UART0_D = UART_receive_n(data, length);
+	__disable_irq(); // global disable IRQs
+	/* Check if interrupt is for TX or RX and handle accordingly
+	 * For TX interrupt we take the data from TX buffer and
+	 * sent it to UART to transmit*/
+        if((UART0_C2 & UART0_C2_TIE_MASK)!=0){
+		uint8_t data;
+		uint16_t i;
+		while(CB_TX->count!=0){
+			CB_RemoveItem(CircBuffT,&data);
+			UART0_D=data;
+        
+        		for (i=2000;i>0;i--);  //wait for it to get sent
 		}
-		UART0_C2 &= ~UART0_C2_RIE_MASK;	
-	}
-	//IF TDRE==1
-	 else if(0 != (UART0_C2 & UART0_C2_TIE_MASK)) 
-	{
-		if(0 != (UART0_S1 & UART0_S1_TDRE_MASK))
-		{
-			UART_send_n(data, UART0_D);
-		} 
+	/*Disabling TX interrupt bit*/
+	
 		UART0_C2 &= ~UART0_C2_TIE_MASK;
-	}	
-	__enable_irq(); /* global disable IRQs */
+   	}
+
+    /* For RX interrupt check add data to the RX buffer and
+     * analyse the data to check if it is an alphabet, number,
+     * punctuation or a miscellaneous characters*/
+	else if((UART0_C2 & UART0_C2_RIE_MASK)!=0 ){
+	        if(UART0_S1 & UART0_S1_RDRF_MASK)!=0){
+			uint8_t c= UART0_D;   
+			rec_data = c;        //updating the global variable
+    			rec_data_count++;
+    			CB_AddItem(CB_RX,&c);
+			/*analyse is a global variable in the project2.h file*/
+    			analyse = 1;     //only setting a flag instead of calling the function as ISR should be as short as possible
+    			UART0_C2|=UART_C2_TIE_MASK;
+		}
+	}
+	__enable_irq(); // global disable IRQs
 	
 }
-
-
-
-	

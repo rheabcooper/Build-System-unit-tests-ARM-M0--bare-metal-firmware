6919133cdfa89dde6c92d475e05fe7d44e283c01
rheabcooper <31560258+rheabcooper@users.noreply.github.com>
2017-10-30 01:40:33 +0530

    Update memory.h

--- project2/include/common/memory.h
+++ project2/include/common/memory.h
@@ -16,6 +16,14 @@
 
 #include "common.h"
 
+
+typedef enum {
+	SUCCESS,
+	NULL_POINTER,
+	FAILED
+}memory_status;
+
+
 /**
  * @brief A function to move data from one memory location to another memory location
  *
@@ -28,21 +36,9 @@
  *
  * @return a pointer to the destination memory location
  */
-uint8_t * my_memmove(uint8_t * src, uint8_t * dst, size_t length);
+int8_t  my_memmove(uint8_t * src, uint8_t * dst, size_t length);
 
-/**
- * @brief A function to copy data from one memory location to another memory location
- *
- * This function takes two byte pointers (one source and one destination)
- * and a length of bytes to copy from the source location to the destination.
- *
- * @param src - pointer data in the source location
- * @param dst - pointer data in the destination location
- * @param length - length of the data in the memory
- *
- * @return a pointer to the destination memory location
- */
-uint8_t * my_memcpy(uint8_t * src, uint8_t * dst, size_t length);
+
 
 /**
  * @brief A function to set a given value to the memory
@@ -56,7 +52,7 @@ uint8_t * my_memcpy(uint8_t * src, uint8_t * dst, size_t length);
  *
  * @return a pointer to the source memory location
  */
-uint8_t * my_memset(uint8_t * src, size_t length, uint8_t value);
+int8_t  my_memset(uint8_t * src, size_t length, uint8_t value);
 
 /**
  * @brief A function to zero out all of the memory
@@ -69,7 +65,7 @@ uint8_t * my_memset(uint8_t * src, size_t length, uint8_t value);
  *
  * @return a pointer to the source memory location
  */
-uint8_t * my_memzero(uint8_t * src, size_t length);
+int8_t  my_memzero(uint8_t * src, size_t length);
 
 /**
  * @brief A function to reverse the order of all of the bytes in the memory
@@ -82,31 +78,8 @@ uint8_t * my_memzero(uint8_t * src, size_t length);
  *
  * @return a pointer to the source in the memory
  */
-uint8_t * my_reverse(uint8_t * src, size_t length);
+int8_t  my_reverse(uint8_t * src, size_t length);
 
 /**
- * @brief A function to allocate the length of words in dynamic memory 
- *
- * This function takes the length of the words to allocate 
- * in dynamic memory. 
- *
- * @param length - length of the words in the memory
- *
- * @return a pointer to the memory allocation if successful or 
- *         a NULL pointer if not successful
- */
-int32_t * reserve_words(size_t length);
-
-/**
- * @brief A function to free a dynamic memory allocation 
- *
- * This function frees up a dynamic memory allocation by
- * providing the pointer src to the free memory function.
- *
- * @param src - pointer data in the source location 
- *
- * @return void
- */
-void free_words(uint32_t * src);
-
+ 
 #endif /* __MEMORY_H__ */

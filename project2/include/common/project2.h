6556a5203667267c045c8fc12123ce7371fb1f22
rheabcooper <31560258+rheabcooper@users.noreply.github.com>
2017-10-30 11:49:11 +0530

    Update project2.h

--- project2/include/common/project2.h
+++ project2/include/common/project2.h
@@ -16,14 +16,6 @@ uint8_t rec_data;
 uint32_t rec_data_count = 0;
 uint8_t analyse = 0;
 
-/**
- * @brief function to run project2 materials
- * 
- * This function calls some various tests that we can run to test 
- * the circular buffer and uart code for the project 2. 
- *
- * @return void
- */
 
 uint32_t CB_TX_length=576;
 uint32_t CB_RX_length=16;
@@ -37,15 +29,78 @@ uint8_t string_for_punc[]="Number of punctuations is : \n\r";
 uint8_t string_for_misc[]="Number of miscellaneous characters is : \n\r";
 
 
-
+/**
+ * @brief A function to transfer an integer from the FRDM to the terminal by converting to ascii
+ *
+ * This function takes the integer value as a parameter
+ *
+ * @param integer_value : the integer to be converted to a parameter value 
+ *
+ */
 void transmit_integer(int32_t integer_value);
+/**
+ * @brief A function to transfer a string from the FRDM to the terminal
+ *
+ * This function takes a pointer to the string as a parameter
+ *
+ * @param *data : pointer to the string
+ *
+ */
 void transmit_string(uint8_t * data);
+/**
+ * @brief A function to transfer a character from the FRDM to the terminal by converting to ascii
+ *
+ * This function takes the character value as a parameter
+ *
+ * @param integer_value : the char to be transmitted 
+ *
+ */
 void transmit_char(uint8_t * data);
+/**
+ * @brief A function to transfer an integer from the FRDM to the terminal by converting to ascii
+ *
+ * This function takes the integer value as a parameter
+ *
+ * @param integer_value : the integer to be converted to a parameter value 
+ *
+ */
+
 void transmit_char_host(uint8_t * data);
+/**
+ * @brief A function to transfer an integer from the FRDM to the terminal by converting to ascii
+ *
+ * This function takes the integer value as a parameter
+ *
+ * @param integer_value : the integer to be converted to a parameter value 
+ *
+ */
+
 void transmit_string_host(uint8_t * data);
-void transmit_integer_host(in32_t integer_value);
+/**
+ * @brief A function to transfer a sstring from the FRDM to the host or BBB
+ *
+ * This function takes a pointer to string
+ *
+ * @param integer_value : pointer to string
+ *
+ */
+void transmit_integer_host(int32_t integer_value);
+/**
+ * @brief A function to categorize the received character into an alphabet,a number or a punctuation
+ *
+ */
 void data_analysis();
 
+
+
+/**
+ * @brief function to run project2 materials
+ * 
+ * This function calls some various tests that we can run to test 
+ * the circular buffer and uart code for the project 2. 
+ *
+ * @return void
+ */
 void project2(void);
 
 #endif /* __PROJECT2_H__ */

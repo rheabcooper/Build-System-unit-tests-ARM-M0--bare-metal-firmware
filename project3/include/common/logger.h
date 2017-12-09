979c8ded49c3a1543ee611a7adcdb8daf2d7258c
rheabcooper <31560258+rheabcooper@users.noreply.github.com>
2017-12-08 18:52:19 -0700

    Update logger.h

--- project3/include/common/logger.h
+++ project3/include/common/logger.h
@@ -1,61 +1,26 @@
 /*
- * logger_queue.h
+ * logger.h
  *
  *  Created on: Dec 3, 2017
  *      Author: rhco4796
  */
 
-#ifndef INCLUDES_LOGGER_QUEUE_H_
-#define INCLUDES_LOGGER_QUEUE_H_
+#ifndef INCLUDES_LOGGER_H_
+#define INCLUDES_LOGGER_H_
 
+void log_integer_host(int32_t integer_value,uint8_t length);
 
-typedef enum logidtype{
-	LOGGER_INITIALIZED,
-	GPIO_INITIALIZED,
-	SYSTEM_INITIALIZED,
-	SYSTEM_HALTED,
-	INFO,
-	WARNING,
-	ERROR,
-	PROFILING_STARTED,
-	PROFILING_RESULT,
-	PROFILING_COMPLETED,
-	DATA_RECEIVED,
-	DATA_ANALYSIS_STARTED,
-	DATA_ALPHA_COUNT,
-	NUMERIC_COUNT,
-	DATA_PUNCTUATION_COUNT,
-	DATA_MISC_COUNT,
-	DATA_ANALYSIS_COMPLETED,
-	HEARTBEAT
-}log_id;
+void flush();
+void log_string_host(uint8_t * data);
 
-typedef struct{
-	log_id logid;
-	uint32_t timestamp;
-	uint8_t loglength;
-	uint8_t *payload;
-	uint8_t checksum;
+void log_data_host(uint8_t * data,uint8_t length);
 
-}Log_t;
+void log_data(uint8_t * data,uint8_t length);
+
+void log_string(uint8_t * data);
+
+void log_integer(uint32_t integer_value,uint8_t length);
 
 
 
-Log_t *main_log;
-
-
-void create_log(Log_t * logger_ptr, log_id ID, uint8_t length, uint8_t *pl);
-void log_item(Log_t * logger_ptr);
-void destroy_log(Log_t *logger_ptr);
-
-
-
-
-
-
-
-
-
-
-
-#endif /* INCLUDES_LOGGER_QUEUE_H_ */
+#endif /* INCLUDES_LOGGER_H_ */

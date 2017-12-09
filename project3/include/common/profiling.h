a4f368c62a5f0b08c8192df560e2ca7e25ac6914
rheabcooper <31560258+rheabcooper@users.noreply.github.com>
2017-12-08 18:53:57 -0700

    Create profiling.h

---
+++ project3/include/common/profiling.h
@@ -0,0 +1,23 @@
+/*
+ * profiling.h
+ *
+ *  Created on: Dec 1, 2017
+ *      Author: rhco4796
+ */
+
+#ifndef INCLUDES_PROFILING_H_
+#define INCLUDES_PROFILING_H_
+
+
+void Timer_init();
+
+void Timer_start();
+
+void Timer_stop();
+
+
+
+
+
+
+#endif /* INCLUDES_PROFILING_H_ */
